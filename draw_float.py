5e5bbd9711b2e5583a4268944999bc910bc88e64
wkcn <wkcn@live.cn>
2022-08-08 02:37:52 +0800

    draw

--- draw_float.py
+++ draw_float.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# [S, E, P] = [1, 5, 2]
-[S, E, P] = [1, 6, 9]
+#[S, E, P] = [1, 5, 2]
+[S, E, P] = [1, 4, 3]
+#[S, E, P] = [1, 6, 9]
+#[S, E, P] = [1, 8, 23]
+#[S, E, P] = [1, 5, 10]
 
 
 
@@ -12,17 +15,37 @@ bias = (2 ** E) / 2 - 1
 
 vs = []
 for i in range(0, 2 ** NUM):
-    minus = (i >> NUM_EP) & 1 
-    e = (i >> P) & ((1 << E) - 1)
-    p = i & ((1 << P) - 1)
-    v = (1 + 2 ** (-p)) * (2 ** (e - bias))
-    if minus:
-        v = -v
+    if i == 0:
+        v = 0
+    else:
+        minus = (i >> NUM_EP) & 1 
+        e = (i >> P) & ((1 << E) - 1)
+        p = i & ((1 << P) - 1)
+        if e == (1 << E) - 1:
+            continue
+        p |= (1 << P)
+        s = str(bin(p))
+        assert s.startswith('0b')
+        s = s[2:]
+        assert len(s) == P + 1, (len(s), P)
+        r = 1.0
+        v = 0
+        for u in s:
+            if u == '1':
+                v += r
+            r *= 0.5
+        v *= (2 ** (e - bias)) - 0
+        if minus:
+            v = -v
     vs.append(v)
 vs.sort()
 
+#vs = list(filter(lambda x: x >= -1000 and x <= 1000, vs))
+
 #plt.hist(vs, bins=NUM)
-# plt.title('FP8 (1-5-2)')
-plt.title('FP16 (1-6-9)')
+#plt.title('FP8 (1-5-2)')
+#plt.title('FP8 (1-4-3)')
+plt.title('FP8 (1-4-3), bias=4')
+#plt.title('FP16 (1-6-9)')
 plt.plot(vs, vs, 'o')
 plt.show()
